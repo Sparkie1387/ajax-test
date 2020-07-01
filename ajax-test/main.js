f60f1492d2acaafce54e6ab9ade2d6cbd4476659
Sparkie1387 <hawkesb77@gmail.com>
2020-07-01 19:09:10 +0000

    initial commit

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,7 +1,7 @@
 const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
   xhr.open("GET", baseURL + type + "/");
   xhr.send();
@@ -14,27 +14,35 @@ function getData(type, cb) {
 }
 
 function getTableHeaders(obj) {
-    var tableHeaders = [];
+  var tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key) {
-        tableHeaders.push('<td>${key}</td>');
-    });
+  Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
 
-    return '<tr>${tableHeaders}</tr>';
+  return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
-    var el = document.getElementById("data");
-    el.innerHTML = "";
+    var tableRows = [];
+  var el = document.getElementById("data");
+  el.innerHTML = "";
 
-    getData(type, function(data) {
-        data = data.results;
-        getTableHeaders(data[0]);
+  getData(type, function (data) {
+    data = data.results;
+    var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+    data.forEach(function (item) {
+        var dataRow = [];
+
+        Object.keys(item).forEach(function(key) {
+            var rowData = item[key].toString();
+            var truncatedData = rowData.substring(0, 15);
+            dataRow.push(`<td>${truncatedData}</td>`);
         });
-
-        el.innerHTML = '<table>${tableHeaders}</table>';
+        tableRows.push(`<tr>${dataRow}</tr>`);
     });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+  });
 }
